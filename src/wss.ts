3dfba390a12bdfde6b1bcb7febacfe4e21f8a939
Muhammad Abdurrahman <rachman.sd@gmail.com>
2025-02-17 00:57:07 +0700

    fix: getWs handle ws unique context by session_id

--- src/wss.ts
+++ src/wss.ts
@@ -1,18 +1,34 @@
 import { HonoWSEvent } from "@/interfaces/hono-event";
 import { WSContext } from "hono/ws";
 import { nanoid } from "nanoid";
-import { createSession, removeSession } from "./session/map";
+import { createSession, getWs, removeSession } from "./session/map";
 
+const sessionMap = new WeakMap<WeakKey, string>();
+// this sould create inside "upgradeWebSocket", so the object not shareable
 export const createWs = (): HonoWSEvent => ({
-  session: {},
   sessionRegister(this: HonoWSEvent, ws: WSContext<unknown>) {
-    const sessionID = "wss" + nanoid(20);
-    this.session.session_id = sessionID;
-    createSession(sessionID, ws);
+    const haSessionID = sessionMap.has(this);
+    if (!haSessionID) {
+      const sessionID = "wss" + nanoid(20);
+      sessionMap.set(this, sessionID);
+      createSession(sessionID, ws);
+    }
   },
   sessionRemove(this: HonoWSEvent) {
-    removeSession(this.session.session_id ?? "");
-    this.session.session_id = undefined;
-    this.session.session_sub = undefined;
+    const sessionID = sessionMap.get(this);
+    if (sessionID) {
+      removeSession(sessionID);
+      sessionMap.delete(this);
+    }
+  },
+  sessionID(this: HonoWSEvent) {
+    return sessionMap.get(this);
+  },
+  getWS(this: HonoWSEvent) {
+    const sessionID = sessionMap.get(this);
+    if (sessionID) {
+      return getWs(sessionID);
+    }
+    return undefined;
   },
 });
