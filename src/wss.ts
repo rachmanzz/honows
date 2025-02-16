cfbfc17bc0dadaeb24f7b49458271254e66ccd81
Muhammad Abdurrahman <rachman.sd@gmail.com>
2025-02-16 21:36:34 +0700

    fix: we do not need promise HonoESEvent

--- src/wss.ts
+++ src/wss.ts
@@ -3,19 +3,16 @@ import { WSContext } from "hono/ws";
 import { nanoid } from "nanoid";
 import { createSession, removeSession } from "./session/map";
 
-export const createWs = () => {
-  const ws: HonoWSEvent | Promise<HonoWSEvent> = {
-    session: {},
-    sessionRegister(this: HonoWSEvent, ws: WSContext<unknown>) {
-      const sessionID = "wss" + nanoid(20);
-      this.session.session_id = sessionID;
-      createSession(sessionID, ws);
-    },
-    sessionRemove(this: HonoWSEvent) {
-      removeSession(this.session.session_id ?? "")
-      this.session.session_id = undefined
-      this.session.session_sub = undefined
-    },
-  };
-  return ws;
-};
+export const createWs = (): HonoWSEvent => ({
+  session: {},
+  sessionRegister(this: HonoWSEvent, ws: WSContext<unknown>) {
+    const sessionID = "wss" + nanoid(20);
+    this.session.session_id = sessionID;
+    createSession(sessionID, ws);
+  },
+  sessionRemove(this: HonoWSEvent) {
+    removeSession(this.session.session_id ?? "");
+    this.session.session_id = undefined;
+    this.session.session_sub = undefined;
+  },
+});
