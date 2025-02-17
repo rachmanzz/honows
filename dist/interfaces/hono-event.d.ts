7ed11f9f0d98d6fdf2bcdff114d7e0df9531f1a9
Muhammad Abdurrahman <rachman.sd@gmail.com>
2025-02-17 22:42:06 +0700

    fix: build version 0.1.5

--- dist/interfaces/hono-event.d.ts
+++ dist/interfaces/hono-event.d.ts
@@ -2,11 +2,12 @@ import type { WSEvents, WSContext } from "hono/ws";
 export type HonoWSEvent<T extends unknown> = WSEvents<T> & {
     sub_id?: string;
     sessionRegister: (ws: WSContext<unknown>) => void;
-    sessionRemove: (sessionID: string) => void;
+    sessionRemove: () => void;
     sessionID: () => string | undefined;
     getWS: () => WSContext<unknown> | undefined;
     subscribe: (channelName: string) => void;
     unsubscribe: (channelName: string) => void;
-    channelSend: (channelName: string, data: string | ArrayBuffer) => void;
+    unsubscribeAll: () => void;
+    channelSend: (channelName: string, data: string | ArrayBuffer | Uint8Array) => void;
 };
 //# sourceMappingURL=hono-event.d.ts.map
