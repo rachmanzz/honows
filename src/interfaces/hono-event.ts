c4d82156b27261900716241bba5f9fbd9fb25662
Muhammad Abdurrahman <rachman.sd@gmail.com>
2025-02-17 22:25:11 +0700

    fix: add type data and function

--- src/interfaces/hono-event.ts
+++ src/interfaces/hono-event.ts
@@ -3,10 +3,11 @@ import type { WSEvents, WSContext } from "hono/ws";
 export type HonoWSEvent <T extends unknown>= WSEvents<T> & {
   sub_id?: string;
   sessionRegister: (ws: WSContext<unknown>) => void;
-  sessionRemove: (sessionID: string) => void;
+  sessionRemove: () => void;
   sessionID: () => string | undefined;
   getWS: () => WSContext<unknown> | undefined;
   subscribe: (channelName: string) => void;
   unsubscribe: (channelName: string) => void;
-  channelSend:(channelName: string, data: string | ArrayBuffer) => void;
+  unsubscribeAll: () => void;
+  channelSend:(channelName: string, data: string | ArrayBuffer | Uint8Array) => void;
 };
